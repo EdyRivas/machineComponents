daaf7125a21233a1b1ea4edf4c05feb51b004b28
EdyRivas <alexicho03@gmail.com>
2022-11-23 20:56:31 -0700

    done

--- src/app/table/table.component.ts
+++ src/app/table/table.component.ts
@@ -11,7 +11,7 @@ export class PeriodicElement {
 }
 
 const ELEMENT_DATA: PeriodicElement[] = [
-  {elemento: '', sensor: 'Hydrogen', ult_Med:  new Date().toLocaleString(), medicion: '',estado: true},
+  // {elemento: '', sensor: 'Hydrogen', ult_Med:  new Date().toLocaleString(), medicion: '',estado: true},
   
 ];
 
@@ -24,7 +24,7 @@ const ELEMENT_DATA: PeriodicElement[] = [
  
 
 export class TableComponent implements OnInit {
-  dataSource = new MatTableDataSource(ELEMENT_DATA); displayedColumns: string[] = [
+  displayedColumns = [
     'elemento', 
     'sensor', 
     'ult_Med', 
@@ -32,11 +32,67 @@ export class TableComponent implements OnInit {
     'estado'
   ];
   // dataSource = new MatTableDataSource(ELEMENT_DATA);
+  dataSource = new MatTableDataSource<PeriodicElement>();
+
   resultsLength = ELEMENT_DATA.length;
   constructor() { }
 
   ngOnInit(): void {
+    this.dataSource.data = ELEMENT_DATA;
+  setTimeout(() => {
+    this.addRow()
+  }, 7500);
   }
+  addRow(){
+    let aux = document.getElementById('auxTable') as HTMLElement;
+    let estandarizador = document.getElementById('tempEstweather') as HTMLElement;
+    let estTemp = estandarizador.innerHTML;
+    let estBool =false;
+    if(parseInt(estTemp)  >= 30 && parseInt(estTemp) <= 53 )
+    estBool = true;
+    let estandData: PeriodicElement = {
+      elemento: 'Estandarizador', sensor: 'Temperatura', ult_Med:  new Date().toLocaleString(), medicion: estTemp,estado: estBool};
+    ELEMENT_DATA.push(estandData);
+    this.dataSource.data = ELEMENT_DATA;
+    
+    let past = document.getElementById('tempPastweather') as HTMLElement;
+    let pastTemp = past.innerHTML;
+    let patBool =false;
+    if(parseInt(pastTemp)  >= 30 && parseInt(pastTemp) <= 53 )
+    patBool = true;
+    let pastData: PeriodicElement = {
+      elemento: 'Pasteurizador', sensor: 'Temperatura', ult_Med:  new Date().toLocaleString(), medicion: pastTemp,estado: patBool};
+    ELEMENT_DATA.push(pastData);
+    this.dataSource.data = ELEMENT_DATA;
+
+    let pastSpeed = document.getElementById('pressPasttext') as HTMLElement;
+    let pSpeed = pastSpeed.innerHTML;
+    let pSBool =false;
+    if(parseInt(pSpeed)  <= 50 )
+    pSBool = true;
+    let pastSData: PeriodicElement = {
+      elemento: 'Pasteurizador', sensor: 'Presión', ult_Med:  new Date().toLocaleString(), medicion: pSpeed,estado: pSBool};
+    ELEMENT_DATA.push(pastSData);
+    this.dataSource.data = ELEMENT_DATA;
+
+    let silo = document.getElementById('pressSilotext') as HTMLElement;
+    let siloSpeed = silo.innerHTML;
+    let siloBool =false;
+    if(parseInt(siloSpeed)  <= 50 )
+    pSBool = true;
+    let siloData: PeriodicElement = {
+      elemento: 'Silos de leche', sensor: 'Temperatura', ult_Med:  new Date().toLocaleString(), medicion: siloSpeed,estado: pSBool};
+    ELEMENT_DATA.push(siloData);
+    this.dataSource.data = ELEMENT_DATA;
+
+    aux.click()
+  }
+  auxRecord(){
+    setTimeout(() => {
+      this. addRow();
+    }, 7500);
+  }
+
   
 }
 export interface Element {
